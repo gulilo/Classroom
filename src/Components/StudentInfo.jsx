d4e1fc4b36b676732f0bda25231ff8f580795c5e
aalony <63516118+aalony@users.noreply.github.com>
2020-09-02 14:59:11 +0300

    studnet data style finished

--- src/Components/StudentInfo.jsx
+++ src/Components/StudentInfo.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Style_MainGrid from "../cssFiles/MainGridLayout.module.css";
+import Style_StudnetInfo from "../cssFiles/StudnetInfoStyle.module.css";
 
 import { MockedContext } from "../MockedContext";
 
@@ -39,7 +40,7 @@ class StudentInfo extends Component {
     const likeAndLikeMe = _.intersection(likes, likeMe);
     const likeAndDislikeMe = _.intersection(likes, dislikeMe);
     const dislikeAndLikeMe = _.intersection(dislikes, likeMe);
-    const dislikeAndDislikeMe = _.intersection(likes, dislikeMe);
+    const dislikeAndDislikeMe = _.intersection(dislikes, dislikeMe);
 
     const likeRest = _.difference(likes, likeAndLikeMe, likeAndDislikeMe);
     const dislikeRest = _.difference(
@@ -47,7 +48,7 @@ class StudentInfo extends Component {
       dislikeAndLikeMe,
       dislikeAndDislikeMe
     );
-
+      console.log (likeAndDislikeMe)
     this.setState({
       likes,
       dislikes,
@@ -66,30 +67,50 @@ class StudentInfo extends Component {
   render() {
     return (
       <div className={Style_MainGrid.AppMainAreaOut}>
-        <h3>likes: </h3>
-        <span>
-          {_.map(this.state.likes, ({ id, name }) => (
-            <li key={id}> {name} </li>
+        <div className={Style_StudnetInfo.StudnetInfoGrid}>
+        <h3 className={Style_StudnetInfo.likeH}>likes: </h3>
+        <span className={Style_StudnetInfo.likeC}>
+          {_.map(this.state.likeAndLikeMe, ({ id, name }) => (
+            <li className={Style_StudnetInfo.LikeLike} key={id}> {name} </li>
+          ))}
+          {_.map(this.state.likeRest, ({ id, name }) => (
+            <li className={Style_StudnetInfo.LikeNone} key={id}> {name} </li>
+          ))}
+          {_.map(this.state.likeAndDislikeMe, ({ id, name }) => (
+            <li className={Style_StudnetInfo.LikeDislike} key={id}> {name} </li>
+          ))}
+
+        </span>
+        
+        
+        <h3 className={Style_StudnetInfo.dislikeH}>dislikes: </h3>
+        <span className={Style_StudnetInfo.dislikeC}>
+          {_.map(this.state.dislikeAndLikeMe, ({ id, name }) => (
+            <li className={Style_StudnetInfo.DislikeLike} key={id}> {name} </li>
+          ))}
+          {_.map(this.state.dislikeRest, ({ id, name }) => (
+            <li className={Style_StudnetInfo.DislikeNone} key={id}> {name} </li>
+          ))}
+          {_.map(this.state.dislikeAndDislikeMe, ({ id, name }) => (
+            <li className={Style_StudnetInfo.DislikeDislike} key={id}> {name} </li>
           ))}
         </span>
-        <h3>dislikes: </h3>
-        <span>
-          {_.map(this.state.dislikes, ({ id, name }) => (
-            <li key={id}> {name} </li>
-          ))}
-        </span>
-        <h3>like me: </h3>
-        <span>
+
+
+
+        <h3 className={Style_StudnetInfo.RlikeH}>like me: </h3>
+        <span className={Style_StudnetInfo.RlikeC}> 
           {_.map(this.state.likeMe, ({ id, name }) => (
             <li key={id}> {name} </li>
           ))}
         </span>
-        <h3>dislikes me: </h3>
-        <span>
+        <h3 className={Style_StudnetInfo.RdislikeH}>dislikes me: </h3>
+        <span className={Style_StudnetInfo.RdislikeC}>
           {_.map(this.state.dislikeMe, ({ id, name }) => (
             <li key={id}> {name} </li>
           ))}
         </span>
+        </div>
       </div>
     );
   }
