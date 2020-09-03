fa564783723421974d56dc245d1d278fe3cc452c
Gal Golan <gulilo@users.noreply.github.com>
2020-09-03 16:05:29 +0300

    Student data

--- src/Routes/StudentRoute.jsx
+++ src/Routes/StudentRoute.jsx
@@ -2,9 +2,18 @@ import React, { Component } from "react";
 
 import StudentEdit from "../Components/StudentEdit";
 import StudentInfo from "../Components/StudentInfo";
+import { MockedContext } from "../MockedContext";
 
 class StudentRoute extends Component {
-  state = {};
+  static contextType = MockedContext;
+
+  state = {
+    newStudent: false,
+    studentInfo: false,
+    studentEdit: false,
+  };
+  getStudentId = () => parseInt(this.props.match.params.studentId, 10);
+  getClassId = () => parseInt(this.props.match.params.classId, 10);
 
   componentDidMount() {
     var newStudent = false;
@@ -22,7 +31,17 @@ class StudentRoute extends Component {
     if (this.state.newStudent) {
       return <StudentEdit newStudent={this.state.newStudent} />;
     } else if (this.state.studentInfo) {
-      return <StudentInfo />;
+      return (
+        <StudentInfo
+          student={this.context.classes.getStudent(
+            this.getClassId(),
+            this.getStudentId()
+          )}
+          otherStudents={
+            this.context.classes.getById(this.getClassId()).students
+          }
+        />
+      );
     } else {
       return <div>somthing</div>;
     }
