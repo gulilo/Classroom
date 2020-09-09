f52a289006451beaed055219e6e637d5eabb7d6e
Gulilo <galgolo@gmail.com>
2020-09-09 10:48:32 +0300

    done with the routes

--- src/Routes/ClassroomDataRoute.jsx
+++ src/Routes/ClassroomDataRoute.jsx
@@ -1,42 +1,36 @@
-import React, { Component } from "react";
+import React, { useState, useContext, useEffect } from "react";
+
 import Style_MainGrid from "../cssFiles/MainGridLayout.module.css";
+
 import ClassData from "../Components/ClassData";
+
 import { MockedContext } from "../MockedContext";
 
-class ClassroomDataRoute extends Component {
-  static contextType = MockedContext;
+function ClassroomDataRoute(props) {
+  const api = useContext(MockedContext);
+  const [classroom, setClassroom] = useState({
+    id: -1,
+    name: "bla",
+    students: [],
+  });
 
-  state = { classroom: { id: -1, name: "bla", students: [] } };
+  const getClassId = () => parseInt(props.match.params.classId, 10);
 
-  getClassId = () => parseInt(this.props.match.params.classId, 10);
+  useEffect(() => {
+    const classroom = api.classes.getById(getClassId());
+    setClassroom(classroom);
+  }, [props.match.params.classId]);
 
-  init = () => {
-    const classroom = this.context.classes.getById(this.getClassId());
-    this.setState({ classroom });
-  };
-
-  componentDidMount() {
-    this.init();
+  if (classroom.id === -1) {
+    return null;
   }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.match.params.classId !== this.props.match.params.classId) {
-      this.init();
-    }
-  }
-
-  render() {
-    if (this.state.classroom.id === -1) {
-      return null;
-    }
-    return (
-      <div className={Style_MainGrid.appClassroomArea}>
-        <div className={Style_MainGrid.AppMainArea}>
-          <ClassData classroom={this.state.classroom} />
-        </div>
+  return (
+    <div className={Style_MainGrid.appClassroomArea}>
+      <div className={Style_MainGrid.AppMainArea}>
+        <ClassData classroom={classroom} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default ClassroomDataRoute;
--- src/Routes/ClassroomRoute.jsx
+++ src/Routes/ClassroomRoute.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useContext, useState } from "react";
 
 import ClassRoomComponent from "../Components/ClassRoomComponent";
 
@@ -6,41 +6,32 @@ import Style_MainGrid from "../cssFiles/MainGridLayout.module.css";
 
 import { MockedContext } from "../MockedContext";
 
-class ClassroomRoute extends Component {
-  static contextType = MockedContext;
+function ClassroomRoute(props) {
+  const api = useContext(MockedContext);
+  const [classroom, setClassroom] = useState({
+    id: -1,
+    name: "bla",
+    students: [],
+  });
 
-  state = { classroom: { id: -1, name: "bla", students: [] } };
+  const getClassId = () => parseInt(props.match.params.classId, 10);
 
-  getClassId = () => parseInt(this.props.match.params.classId, 10);
+  useEffect(() => {
+    const classroom = api.classes.getById(getClassId());
+    setClassroom(classroom);
+  });
 
-  init = () => {
-    const classroom = this.context.classes.getById(this.getClassId());
-    this.setState({ classroom });
-  };
-
-  componentDidMount() {
-    this.init();
+  if (classroom.id === -1) {
+    return null;
   }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.match.params.classId !== this.props.match.params.classId) {
-      this.init();
-    }
-  }
-
-  render() {
-    if (this.state.classroom.id === -1) {
-      return null;
-    }
-
-    return (
-      <div className={Style_MainGrid.appClassroomArea}>
-        <div className={Style_MainGrid.AppMenuArea2}>
-          <ClassRoomComponent classroom={this.state.classroom} />
-        </div>
+  return (
+    <div className={Style_MainGrid.appClassroomArea}>
+      <div className={Style_MainGrid.AppMenuArea2}>
+        <ClassRoomComponent classroom={classroom} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default ClassroomRoute;
