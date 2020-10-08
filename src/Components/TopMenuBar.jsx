c775ea14e7eea1b1b70818bdfe4babacf4261ebe
Gal Golan <galgolo@gmail.com>
2020-10-08 16:11:31 +0300

    removed Router from TopMenuBar

--- src/Components/TopMenuBar.jsx
+++ src/Components/TopMenuBar.jsx
@@ -1,29 +1,26 @@
 import AppColors from "../cssFiles/Colors.module.css";
 import style from "../cssFiles/TopMenuBar.module.css";
 
-import { BrowserRouter as Router,NavLink } from "react-router-dom";
+import { BrowserRouter as Router, NavLink } from "react-router-dom";
 import Greeting from "./Greeting";
 
-import React from 'react'
+import React from "react";
 
 export default function TopMenuBar() {
-    return (
-        <Router>
-          <div className={style.TopMenuBar}>
-            <div className={style.ClassMenuItems}>
+  return (
+    <div className={style.TopMenuBar}>
+      <div className={style.ClassMenuItems}>
         <NavLink to="/classroom" className={style.MenuItem}>
           <span className={style.TextField}>קבוצות</span>
         </NavLink>
         <NavLink to="/classroom/new" className={style.MenuItem}>
-        <span className={style.TextField}>תלמידים</span>
+          <span className={style.TextField}>תלמידים</span>
         </NavLink>
-        
-            </div>
-        <br />
-        <div className={style.Greeting}>
+      </div>
+      <br />
+      <div className={style.Greeting}>
         <Greeting name="Degenerat" />
-        </div>
-        </div>
-      </Router>
-    )
+      </div>
+    </div>
+  );
 }
